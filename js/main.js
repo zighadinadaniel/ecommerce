4fb8a85b9f485776480feacf3c7f7e661f3b4ba0
zighadinadaniel <danielzighadina12345@gmail.com>
2023-02-24 13:56:18 +0100

    second commit of the code base

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Cart
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+// Open Cart
+cartIcon.onclick = () =>{
+    cart.classList.add('active')
+}
+
+ 
